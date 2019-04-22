a2734f79517dc783af597db80a4eff7d303563de
qtwutwu <33755078+qtwutwu@users.noreply.github.com>
2019-04-22 19:27:36 +0300

    Update tests.py

--- snake/tests.py
+++ snake/tests.py
@@ -1,1 +1,20 @@
+import unittest
+import curses
+from snake import sn
 
+class TestSnake(unittest.TestCase):
+
+    def setUp(self):
+        self.body = ([10, 10], [10, 9], [10, 8])
+        self.direction = curses.KEY_RIGHT
+        self.eaten_food = []
+
+    def test_set_direction(self):
+        '''Testing add menthod'''
+        sn.set_direction(self, curses.KEY_UP)
+
+        self.assertEqual(sn.direction,curses.KEY_UP)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
